d118397cdcf870fdd8257401ccd3661ac9998401
Sudeshna Kundu <sudeshna.buie@gmail.com>
2024-12-29 19:19:55 -0700

    Modify setup.sh Commit

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
-# setup.sh
+#!/bin/bash
+
+# Install NLTK first to avoid missing module errors
+pip install nltk
+
+# Download required NLTK data
 mkdir -p ~/.nltk_data
 python -m nltk.downloader -d ~/.nltk_data punkt stopwords
