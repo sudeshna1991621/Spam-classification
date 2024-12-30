7ecc944a4c694cc2fd28b0269e2d1311a499c7ba
Sudeshna Kundu <sudeshna.buie@gmail.com>
2024-12-30 13:19:48 -0700

    Final Commit

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install NLTK data (specifically the 'punkt' tokenizer)
-python -m nltk.downloader punkt
-
-# Any other setup commands you need, e.g., install dependencies
+# Install necessary dependencies
 pip install -r requirements.txt
+
+# Download NLTK data (including 'punkt')
+python -m nltk.downloader punkt
